3aedad75ffc7d3af464edee4bdb1b9efad7bdfaf
kashifameen <36233578+kashifameen@users.noreply.github.com>
2021-03-31 10:49:57 +0100

    Update main.php

--- main.php
+++ main.php
@@ -1,16 +1,13 @@
-
 <html>
 <link rel="stylesheet" href="style.css">
 <h1> My Car Collection</h1>
 </html>
-
 <?php
 $db = new PDO('mysql:host=db; dbname=collectionDB', 'root', 'password');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 $query = $db->prepare('SELECT `Brand`, `Model`, `Year_released` FROM `collectionapp`;');
 $query->execute();
 $result = $query->fetchAll();
-
 require_once 'functions.php';
 echo carTable($result);
 ?>
