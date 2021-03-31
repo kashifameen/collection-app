042b8b51f7538595918610997a75ba57cfaa1b21
Kashif Ameen <kashu@live.co.uk>
2021-03-31 12:22:43 +0100

    removed more space

--- functions.php
+++ functions.php
@@ -13,13 +13,13 @@ function connectToDB(): object
 
 function getAllFromDB(object $db): array
 {
-    $query = $db->prepare('SELECT * FROM `collectionapp`;');
+    $query= $db->prepare('SELECT * FROM `collectionapp`;');
     $query->execute();
     return $query->fetchAll();
 }
 function carTable(array $query):string
 {
-    $carTableString = '<div class= "container">';
+    $carTableString= '<div class= "container">';
     foreach ($query as $user){
         $carTableString .= '<div>'. 'Brand: '. $user['Brand'] . '<br>' .'Model: '. $user['Model'] . '<br>'. 'Year Released: '. $user['Year_released']. '</div>';
     }
--- main.php
+++ main.php
@@ -5,12 +5,12 @@
 <body>
 <h1> My Car Collection</h1>
 
-<div class= "button"><button class = "addtocollectionbtn"><a href = "addtocollection.php">Add a new car to the collection</a></button></div>
+<div class= "button"><button class= "addtocollectionbtn"><a href= "addtocollection.php">Add a new car to the collection</a></button></div>
 
 <?php
 require_once 'functions.php';
-$db = connectToDB();
-$query = getAllFromDB($db);
+$db= connectToDB();
+$query= getAllFromDB($db);
 echo carTable($query);
 ?>
 
