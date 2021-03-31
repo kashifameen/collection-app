44c86208b4e08f3e6af6f73fe0ede190e3cad7d1
Kashif Ameen <kashu@live.co.uk>
2021-03-31 10:23:15 +0100

    removed spacing

--- functions.php
+++ functions.php
@@ -6,8 +6,6 @@ function carTable(array $result):string
         $carTableString .= '<div>'. 'Brand: '. $user['Brand'] . '<br>' .'Model: '. $user['Model'] . '<br>'. 'Year Released: '. $user['Year_released']. '</div>';
     }
     $carTableString .= '</div>';
-
     return $carTableString;
 }
-
 ?>
